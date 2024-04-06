3b8aeefd8eb964410a6764a55edc024e07f8a072
Shubhajit Roy <shubhajitroy123@gmail.com>
2024-04-06 13:19:57 +0530

    Updated the network.py and train.py files

--- network.py
+++ network.py
@@ -3,11 +3,12 @@ import torch.nn.functional as F
 from torch_geometric.nn import GCNConv
 
 
-class Net(torch.nn.Module):
+class Net1(torch.nn.Module):
     def __init__(self, args):
-        super(Net, self).__init__()
+        super(Net1, self).__init__()
         self.conv1 = GCNConv(args.num_features, args.hidden)
-        self.conv2 = GCNConv(args.hidden, args.num_classes)
+        self.conv2 = GCNConv(args.hidden, args.hidden)
+        self.lt1 = torch.nn.Linear(args.hidden, args.num_classes)
 
     def reset_parameters(self):
         self.conv1.reset_parameters()
@@ -19,5 +20,37 @@ class Net(torch.nn.Module):
         x = F.relu(x)
         x = F.dropout(x, training=self.training)
         x = self.conv2(x, edge_index)
+        x = F.relu(x)
+        x = F.dropout(x, training=self.training)
+        z = self.lt1(x)
+        return F.log_softmax(z, dim=1), x
+    
+class Net2(torch.nn.Module):
+    def __init__(self, args):
+        super(Net2, self).__init__()
+        self.w0 = torch.nn.Parameter(torch.Tensor(args.num_features, 1))
+        self.b0 = torch.nn.Parameter(torch.Tensor(args.num_features, args.num_features))
+        self.conv1 = GCNConv(args.num_features, args.hidden)
+        self.conv2 = GCNConv(args.hidden, args.hidden)
+        self.lt1 = torch.nn.Linear(args.hidden, args.num_classes)
 
+
+    def reset_parameters(self):
+        torch.nn.init.xavier_uniform_(self.w0)
+        torch.nn.init.xavier_uniform_(self.b0)
+        self.conv1.reset_parameters()
+        self.conv2.reset_parameters()
+
+    def forward(self, x, edge_index, E_meta):
+        new_w0 = torch.matmul(self.w0, E_meta)
+        new_w0 = torch.add(new_w0, self.b0)
+        new_w0 = F.relu(new_w0)
+        x = torch.matmul(x, new_w0)
+        x = self.conv1(x, edge_index)
+        x = F.relu(x)
+        x = F.dropout(x, training=self.training)
+        x = self.conv2(x, edge_index)
+        x = F.relu(x)
+        x = F.dropout(x, training=self.training)
+        x = self.lt1(x)
         return F.log_softmax(x, dim=1)
--- train.py
+++ train.py
@@ -2,25 +2,65 @@ import argparse
 import torch.nn.functional as F
 import torch
 from torch import tensor
-from network import Net
+from network import Net1, Net2
 import numpy as np
 from utils import load_data, coarsening
 import os
 
-def train_M1(model, x, edge_index, mask, y, loss_fn, optimizer, metric_fn):
+def train_M1(model, x, edge_index, mask, y, loss_fn, optimizer):
     model.train()
     optimizer.zero_grad()
-    out = model(x, edge_index)
+    out, E_meta = model(x, edge_index)
     loss = loss_fn(out[mask], y[mask])
     loss.backward()
     optimizer.step()
-    return loss.item(), metric_fn(out[mask], y[mask])
+    return loss.item()
 
 def infer_M1(model, x, edge_index, mask, y, loss_fn, metric_fn):
     model.eval()
-    out = model(x, edge_index)
+    out, E_meta = model(x, edge_index)
     loss = loss_fn(out[mask], y[mask])
-    return loss.item(), metric_fn(out[mask], y[mask])
+    return E_meta, loss.item(), metric_fn(out[mask], y[mask])
+
+def train_M2(model, graphs, E_meta, loss_fn, optimizer):
+    total_loss = 0
+    for graph in graphs:
+        model.train()
+        optimizer.zero_grad()
+        x = graph.x.to(device)
+        edge_index = graph.edge_index.to(device)
+        E_meta = E_meta[graph.idx].to(device)
+        out = model(x, edge_index, E_meta)
+        loss = loss_fn(out[graph.train_mask], graph.y[graph.train_mask])
+        loss.backward()
+        optimizer.step()
+        total_loss += loss.item()
+    return total_loss / len(graphs)
+
+def infer_M2(model, graphs, E_meta, loss_fn, metric_fn, infer_type):
+    total_loss = 0
+    all_out = []
+    all_label = []
+    for graph in graphs:
+        model.eval()
+        x = graph.x.to(device)
+        edge_index = graph.edge_index.to(device)
+        E_meta = E_meta[graph.idx].to(device)
+        out = model(x, edge_index, E_meta)
+        if infer_type == 'test':
+            loss = loss_fn(out[graph.test_mask], graph.y[graph.test_mask])
+            all_out.append(out[graph.test_mask])
+            all_label.append(graph.y[graph.test_mask])
+        else:
+            loss = loss_fn(out[graph.val_mask], graph.y[graph.val_mask])
+            all_out.append(out[graph.val_mask])
+            all_label.append(graph.y[graph.val_mask])
+        total_loss += loss.item()
+    all_out = torch.cat(all_out, dim=0)
+    all_label = torch.cat(all_label, dim=0)
+    return total_loss / len(graphs), metric_fn(all_out, all_label)
+        
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -28,7 +68,8 @@ if __name__ == '__main__':
     parser.add_argument('--experiment', type=str, default='fixed') #'fixed', 'random', 'few'
     parser.add_argument('--runs', type=int, default=20)
     parser.add_argument('--hidden', type=int, default=64)
-    parser.add_argument('--epochs', type=int, default=60)
+    parser.add_argument('--epochs1', type=int, default=60)
+    parser.add_argument('--epochs2', type=int, default=100)
     parser.add_argument('--early_stopping', type=int, default=10)
     parser.add_argument('--lr', type=float, default=0.01)
     parser.add_argument('--weight_decay', type=float, default=0.0005)
@@ -42,13 +83,13 @@ if __name__ == '__main__':
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     args.num_features, args.num_classes, candidate, C_list, Gc_list, map_list = coarsening(args.dataset, 1-args.coarsening_ratio, args.coarsening_method)
-    model1 = Net(args).to(device)
-    model2 = Net(args).to(device)
+    model1 = Net1(args).to(device)
+    model2 = Net2(args).to(device)
     all_acc = []
 
     for i in range(args.runs):
 
-        data, coarsen_features, coarsen_train_labels, coarsen_train_mask, coarsen_val_labels, coarsen_val_mask, coarsen_edge = load_data(
+        data, coarsen_features, coarsen_train_labels, coarsen_train_mask, coarsen_val_labels, coarsen_val_mask, coarsen_edge, graphs = load_data(
             args.dataset, candidate, C_list, Gc_list, args.experiment, map_list)
         data = data.to(device)
         coarsen_features = coarsen_features.to(device)
@@ -64,28 +105,40 @@ if __name__ == '__main__':
 
         model1.reset_parameters()
         optimizer1 = torch.optim.Adam(model1.parameters(), lr=args.lr, weight_decay=args.weight_decay)
+        model2.reset_parameters()
+        optimizer2 = torch.optim.Adam(model2.parameters(), lr=args.lr, weight_decay=args.weight_decay)
 
-        best_val_loss = float('inf')
-        val_loss_history = []
-        for epoch in range(args.epochs):
-            train_loss, train_acc = train_M1(model1, coarsen_features, coarsen_edge, coarsen_train_mask, coarsen_train_labels, F.nll_loss, optimizer1, metric_fn=lambda x, y: int(x.max(1)[1].eq(y).sum().item()) / int(y.sum()))
-            val_loss, val_acc = infer_M1(model1, coarsen_features, coarsen_edge, coarsen_val_mask, coarsen_val_labels, F.nll_loss, metric_fn=lambda x, y: int(x.max(1)[1].eq(y).sum().item()) / int(y.sum()))
+        best_val_loss_M1 = float('inf')
+        best_val_loss_M2 = float('inf')
+        val_loss_history_M1 = []
+        val_loss_history_M2  = []
+        for epoch in range(args.epochs1):
+            train_loss = train_M1(model1, coarsen_features, coarsen_edge, coarsen_train_mask, coarsen_train_labels, F.nll_loss, optimizer1)
+            E_meta, val_loss, val_acc = infer_M1(model1, coarsen_features, coarsen_edge, coarsen_val_mask, coarsen_val_labels, F.nll_loss, metric_fn=lambda x, y: int(x.max(1)[1].eq(y).sum().item()) / int(y.sum()))
 
-            if val_loss < best_val_loss and epoch > args.epochs // 2:
-                best_val_loss = val_loss
-                torch.save(model1.state_dict(), path + 'checkpoint-best-acc.pkl')
+            if val_loss < best_val_loss_M1:
+                best_val_loss_M1 = val_loss
+                torch.save(model1.state_dict(), path + 'checkpoint-best-loss-model-1.pkl')
+            val_loss_history_M1.append(val_loss)
+            if epoch > args.early_stopping and val_loss_history_M1[-1] > val_loss_history_M1[-args.early_stopping]:
+                break
+        
+        for epoch in range(args.epochs2):
+            model1.load_state_dict(torch.load(path + 'checkpoint-best-loss-model-1.pkl'))
+            E_meta, val_loss, val_acc = infer_M1(model1, coarsen_features, coarsen_edge, coarsen_val_mask, coarsen_val_labels, F.nll_loss, metric_fn=lambda x, y: int(x.max(1)[1].eq(y).sum().item()) / int(y.sum()))
 
-            val_loss_history.append(val_loss)
-            if args.early_stopping > 0 and epoch > args.epochs // 2:
-                tmp = tensor(val_loss_history[-(args.early_stopping + 1):-1])
-                if val_loss > tmp.mean().item():
-                    break
+            train_loss = train_M2(model2, graphs, E_meta, F.nll_loss, optimizer2)
+            val_loss, val_acc = infer_M2(model2, graphs, E_meta, F.nll_loss, metric_fn=lambda x, y: int(x.max(1)[1].eq(y).sum().item()) / int(y.sum()), infer_type='val')
 
-        model.load_state_dict(torch.load(path + 'checkpoint-best-acc.pkl'))
-        model.eval()
-        pred = model(data.x, data.edge_index).max(1)[1]
-        test_acc = int(pred[data.test_mask].eq(data.y[data.test_mask]).sum().item()) / int(data.test_mask.sum())
-        print(test_acc)
+            if val_loss < best_val_loss_M2:
+                best_val_loss_M2 = val_loss
+                torch.save(model2.state_dict(), path + 'checkpoint-best-loss-model-2.pkl')
+            if epoch > args.early_stopping and val_loss_history_M2[-1] > val_loss_history_M2[-args.early_stopping]:
+                break
+
+        model1.load_state_dict(torch.load(path + 'checkpoint-best-loss-model-1.pkl'))
+        model2.load_state_dict(torch.load(path + 'checkpoint-best-loss-model-2.pkl'))
+        test_loss, test_acc = infer_M2(model2, graphs, E_meta, F.nll_loss, metric_fn=lambda x, y: int(x.max(1)[1].eq(y).sum().item()) / int(y.sum()), infer_type='test')
         all_acc.append(test_acc)
 
     print('ave_acc: {:.4f}'.format(np.mean(all_acc)), '+/- {:.4f}'.format(np.std(all_acc)))
