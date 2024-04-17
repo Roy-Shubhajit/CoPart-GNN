Updated to run faster on GPU and fixed masking

train.py
@@ -45,13 +45,13 @@ def infer_M1(model, x, edge_index, mask, y, loss_fn):
 def train_M2(model, graph_data, loss_fn, optimizer):
     total_loss = 0
     for graph in graph_data:
-        model.train()
-        optimizer.zero_grad()
-        x = graph.x.to(device)
-        y = graph.y.to(device)
-        edge_index = graph.edge_index.to(device)
-        out = model(x, edge_index)
         if True in graph.train_mask:
+            model.train()
+            optimizer.zero_grad()
+            x = graph.x.to(device)
+            y = graph.y.to(device)
+            edge_index = graph.edge_index.to(device)
+            out = model(x, edge_index)
             loss = loss_fn(out[graph.train_mask], y[graph.train_mask])
             loss.backward(retain_graph=True)
             optimizer.step()
@@ -69,9 +69,9 @@ def infer_M2(model, graph_data, loss_fn, infer_type):
         x = graph.x.to(device)
         y = graph.y.to(device)
         edge_index = graph.edge_index.to(device)
-        out = model(x, edge_index)
         if infer_type == 'test':
             if True in graph.test_mask:
+                out = model(x, edge_index)
                 loss = loss_fn(out[graph.test_mask], y[graph.test_mask])
                 total_loss += loss.item()
                 all_out = torch.cat((all_out, torch.max(out[graph.test_mask], dim=1)[1].to(device)), dim=0)
@@ -80,6 +80,7 @@ def infer_M2(model, graph_data, loss_fn, infer_type):
                 continue
         else:
             if True in graph.val_mask:
+                out = model(x, edge_index)
                 loss = loss_fn(out[graph.val_mask], y[graph.val_mask])
                 total_loss += loss.item()
                 all_out = torch.cat((all_out, torch.max(out[graph.val_mask], dim=1)[1].to(device)), dim=0)
@@ -187,7 +188,7 @@ if __name__ == '__main__':
 
         best_model2 = model2.load_state_dict(torch.load(path+'/model2.pt'))
         test_loss, test_acc = infer_M2(model2, graph_data, new_loss, 'test')
-        writer.add_scalar('Test Acc', test_acc, 0)
+        writer.add_scalar('Model 2 - Accuracy/test', test_acc, i)
         all_acc.append(test_acc)
         print(f"Run {i+1}/{args.runs} - Test Loss: {test_loss:.4f}, Test Accuracy: {test_acc:.4f}")
         print("#####################################################################")

utils.py
@@ -21,10 +21,18 @@ def create_distribution_tensor(input_tensor, class_count):
 def one_hot(x, class_count):
     return torch.eye(class_count)[x, :]
 
-def neighbour(G, node):
-    edges_connected_to_k = torch.nonzero(G.edge_index[0] == node, as_tuple=False)
-    neighbors_k = G.edge_index[1][edges_connected_to_k].flatten().tolist()
-    return neighbors_k
+def neighbor(G, nodes):
+    if not hasattr(G, 'edge_index'):
+        raise ValueError("The input graph must have the 'edge_index' attribute.")
+    if not isinstance(nodes, torch.Tensor):
+        nodes = torch.tensor(nodes)
+    nodes = nodes.to(device)
+    edge_index_gpu = G.edge_index.to(nodes.device)
+    mask = (edge_index_gpu[0] == nodes.unsqueeze(-1)).any(dim=0)
+    edges_connected_to_nodes = edge_index_gpu[:, mask]
+    neighbors = torch.unique(edges_connected_to_nodes[1])
+
+    return neighbors
 
 def extract_components(H):
         if H.A.shape[0] != H.A.shape[1]:
@@ -94,11 +102,13 @@ def coarsening(args, coarsening_ratio, coarsening_method):
     else:
         dataset = Planetoid(root='./dataset', name=args.dataset)
     data = dataset[0]
+    num_classes = len(set(np.array(data.y)))
     if args.normalize_features:
         data.x = torch.nn.functional.normalize(data.x, p=1)
     G = gsp.graphs.Graph(W=to_dense_adj(data.edge_index)[0])
     components = extract_components(G)
     candidate = sorted(components, key=lambda x: len(x.info['orig_idx']), reverse=True)
+    data.to(device)
     number = 0
     C_list=[]
     Gc_list=[]
@@ -113,38 +123,44 @@ def coarsening(args, coarsening_ratio, coarsening_method):
                 Gc_list.append(Gc)
             mapping_dict = metanode_to_node_mapping_new(subgraph_mapping(mapping_dict_list), original_map)
             for key, value in mapping_dict.items():
-                ext_nodes = []
+                value = torch.LongTensor(value).to(device)
                 if args.extra_node:
-                    for node in value:
-                        ext_nodes.extend(neighbour(data, node))
-                    ext_nodes = list(set(ext_nodes))
-                    value.extend(ext_nodes)
-                    value = list(set(value))
-                value = torch.LongTensor(value)
-                M = data.subgraph(index_to_mask(value, data.num_nodes))
-                M.num_classes = len(set(np.array(data.y)))
-                M.map_dict = {int(v): i for i, v in enumerate(value)}
-                M.ext_node = ext_nodes
+                    ext_nodes = neighbor(data, value)
+                    actual_ext = ext_nodes[~torch.isin(ext_nodes, value)].to(device)
+                    value = torch.cat((value, ext_nodes), dim=0)
+                    value = torch.unique(value)
+                value, _ = torch.sort(value)
+                mappiing = {}
+                for i in range(len(value)):
+                    mappiing[value[i].item()] = i
+                M = data.subgraph(value)
+                M.num_classes = num_classes
+                M.map_dict = mappiing
+                M.ext_node = actual_ext
+                M.to(device)
                 subgraph_list.append(M)
         else:
             mapping_dict = metanode_to_node_mapping_new(subgraph_mapping([{0: 0}]), original_map)
             for key, value in mapping_dict.items():
-                ext_nodes = []
+                value = torch.LongTensor(value).to(device)
                 if args.extra_node:
-                    for node in value:
-                        ext_nodes.extend(neighbour(data, node))
-                    ext_nodes = list(set(ext_nodes))
-                    value.extend(ext_nodes)
-                    value = list(set(value))
-                value = torch.LongTensor(value)
-                M = data.subgraph(index_to_mask(value, data.num_nodes))
-                M.num_classes = len(set(np.array(data.y)))
-                M.map_dict = {int(v): i for i, v in enumerate(value)}
-                M.ext_node = ext_nodes
+                    ext_nodes = neighbor(data, value)
+                    actual_ext = ext_nodes[~torch.isin(ext_nodes, value)].to(device)
+                    value = torch.cat((value, ext_nodes), dim=0)
+                    value = torch.unique(value)
+                value, _ = torch.sort(value)
+                mappiing = {}
+                for i in range(len(value)):
+                    mappiing[value[i].item()] = i
+                M = data.subgraph(value)
+                M.num_classes = num_classes
+                M.map_dict = mappiing
+                M.ext_node = actual_ext
+                M.to(device)
                 subgraph_list.append(M)
         number += 1
     print("Subgraphs created, number of subgraphs: ", len(subgraph_list))
-    return data.x.shape[1], len(set(np.array(data.y))), candidate, C_list, Gc_list, subgraph_list
+    return data.x.shape[1], num_classes, candidate, C_list, Gc_list, subgraph_list
 
 def index_to_mask(index, size):
     mask = torch.zeros(size, dtype=torch.bool, device=index.device)