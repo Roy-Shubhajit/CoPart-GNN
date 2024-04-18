Updated code to work on GPU and fixed masking

graph_coarsening/coarsening_utils.py
@@ -147,17 +147,19 @@ def coarsen(
         n = Gc.N
         new_num = 0
         in_list = False
-        for i in range(G.N):
+        for i in range(N):
             for sublist in coarsening_list:
-                if i in sublist and i == sublist[0]:
-                    mapping_dict[i] = new_num
-                    new_num += 1
-                    in_list = True
-                    break
-                elif i in sublist and i != sublist[0]:
-                    mapping_dict[i] = mapping_dict[sublist[0]]
-                    in_list = True
-                    break
+                if i in sublist:
+                    if any(j in mapping_dict for j in sublist):
+                        common = np.intersect1d(sublist, list(mapping_dict.keys()))
+                        mapping_dict[i] = mapping_dict[common.item()]
+                        in_list = True
+                        break
+                    else:
+                        mapping_dict[i] = new_num
+                        new_num += 1
+                        in_list = True
+                        break
                 else:
                     in_list = False
             if not in_list:

train.py
@@ -7,6 +7,8 @@ import numpy as np
 from utils import load_data, coarsening, create_distribution_tensor
 import os
 from tqdm import tqdm
+from torch_geometric.loader import DataLoader
+from torch.utils.tensorboard import SummaryWriter
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -51,8 +53,8 @@ def train_M2(model, graphs, E_meta, loss_fn, optimizer):
         y = graph.y.to(device)
         edge_index = graph.edge_index.to(device)
         E_meta_pass = E_meta[graph.meta_idx].to(device)
-        out = model(x, edge_index, E_meta_pass)
         if True in graph.train_mask:
+            out = model(x, edge_index, E_meta_pass)
             loss = loss_fn(out[graph.train_mask], y[graph.train_mask])
             loss.backward(retain_graph=True)
             optimizer.step()
@@ -71,9 +73,9 @@ def infer_M2(model, graphs, E_meta, loss_fn, infer_type):
         y = graph.y.to(device)
         edge_index = graph.edge_index.to(device)
         E_meta_pass = E_meta[graph.meta_idx].to(device)
-        out = model(x, edge_index, E_meta_pass)
         if infer_type == 'test':
             if True in graph.test_mask:
+                out = model(x, edge_index, E_meta_pass)
                 loss = loss_fn(out[graph.test_mask], y[graph.test_mask])
                 total_loss += loss.item()
                 all_out = torch.cat((all_out, torch.max(out[graph.test_mask], dim=1)[1].to(device)), dim=0)
@@ -82,6 +84,7 @@ def infer_M2(model, graphs, E_meta, loss_fn, infer_type):
                 continue
         else:
             if True in graph.val_mask:
+                out = model(x, edge_index, E_meta_pass)
                 loss = loss_fn(out[graph.val_mask], y[graph.val_mask])
                 total_loss += loss.item()
                 all_out = torch.cat((all_out, torch.max(out[graph.val_mask], dim=1)[1].to(device)), dim=0)
@@ -101,15 +104,16 @@ if __name__ == '__main__':
     parser.add_argument('--hidden', type=int, default=256)
     parser.add_argument('--epochs1', type=int, default=30)
     parser.add_argument('--epochs2', type=int, default=200)
-    parser.add_argument('--num_layers1', type=int, default=10)
-    parser.add_argument('--num_layers2', type=int, default=10)
+    parser.add_argument('--num_layers1', type=int, default=2)
+    parser.add_argument('--num_layers2', type=int, default=2)
+    parser.add_argument('--batch_size', type=int, default=1)
     parser.add_argument('--early_stopping', type=int, default=10)
     parser.add_argument('--extra_node', type=bool, default=False)
     parser.add_argument('--lr', type=float, default=0.001)
     parser.add_argument('--weight_decay', type=float, default=0.0005)
     parser.add_argument('--normalize_features', type=bool, default=True)
     parser.add_argument('--coarsening_ratio', type=float, default=0.5)
-    parser.add_argument('--coarsening_method', type=str, default='variation_neighborhoods')
+    parser.add_argument('--coarsening_method', type=str, default='variation_neighborhoods') #'variation_neighborhoods', 'variation_edges', 'variation_cliques', 'heavy_edge', 'algebraic_JC', 'affinity_GS', 'kron'
     parser.add_argument('--output_dir', type=str, required=True)
     args = parser.parse_args()
     path = "save/"+args.output_dir+"/"
@@ -117,7 +121,8 @@ if __name__ == '__main__':
         os.makedirs('save')
     if not os.path.exists(path):
         os.makedirs(path)
-    args.num_features, args.num_classes, candidate, C_list, Gc_list, map_list = coarsening(args.dataset, 1-args.coarsening_ratio, args.coarsening_method)
+    writer = SummaryWriter(path)
+    args.num_features, args.num_classes, candidate, C_list, Gc_list, map_list = coarsening(args, args.dataset, 1-args.coarsening_ratio, args.coarsening_method)
     print('num_features: {}, num_classes: {}'.format(args.num_features, args.num_classes))
     print('Number of components: {}'.format(len(candidate)))
     model1 = Net1(args).to(device)
@@ -126,9 +131,9 @@ if __name__ == '__main__':
 
     for i in range(args.runs):
         print(f"####################### Run {i+1}/{args.runs} #######################")
-        data, coarsen_features, coarsen_train_labels, coarsen_train_mask, coarsen_val_labels, coarsen_val_mask, coarsen_test_labels, coarsen_test_mask, coarsen_edge, graphs = load_data(args, 
+        run_writer = SummaryWriter(path+'/run_'+str(i+1))
+        coarsen_features, coarsen_train_labels, coarsen_train_mask, coarsen_val_labels, coarsen_val_mask, coarsen_test_labels, coarsen_test_mask, coarsen_edge, graphs = load_data(args, 
             args.dataset, candidate, C_list, Gc_list, args.experiment, map_list)
-        data = data.to(device)
         coarsen_features = coarsen_features.to(device)
         coarsen_train_labels = coarsen_train_labels.to(device)
         coarsen_train_mask = coarsen_train_mask.to(device)
@@ -140,9 +145,8 @@ if __name__ == '__main__':
 
         if args.normalize_features:
             coarsen_features = F.normalize(coarsen_features, p=1)
-            data.x = F.normalize(data.x, p=1)
-            for graph in graphs:
-                graph.x = F.normalize(graph.x, p=1)
+        
+        graph_data = DataLoader(graphs, batch_size=args.batch_size, shuffle=True)  
 
         model1.reset_parameters()
         optimizer1 = torch.optim.Adam(model1.parameters(), lr=args.lr, weight_decay=args.weight_decay)
@@ -159,13 +163,17 @@ if __name__ == '__main__':
         for epoch in tqdm(range(args.epochs1), desc='Training Model 1',ascii=True):
             train_loss = train_M1(model=model1, x=coarsen_features, edge_index=coarsen_edge, mask=coarsen_train_mask, y=coarsen_train_labels, loss_fn=F.l1_loss, optimizer=optimizer1)
             E_meta, val_loss = infer_M1(model=model1, x=coarsen_features, edge_index=coarsen_edge, mask=coarsen_val_mask, y=coarsen_val_labels, loss_fn=F.l1_loss)
-            if (epoch+1)%5 == 0 or epoch == 0:
-                print(f"Epoch {epoch+1}/{args.epochs1} - Train Loss: {train_loss:.4f}, Val Loss: {val_loss:.4f}")
+            #if (epoch+1)%5 == 0 or epoch == 0:
+                
             if val_loss < best_val_loss_M1 or epoch == 0:
+                print(f"Epoch {epoch+1}/{args.epochs1} - Train Loss: {train_loss:.4f}, Val Loss: {val_loss:.4f}")
                 best_val_loss_M1 = val_loss
                 torch.save(model1.state_dict(), path + 'checkpoint-best-loss-model-1.pkl')
             val_loss_history_M1.append(val_loss)
+            run_writer.add_scalar('Model 1 - Loss/train', train_loss, epoch)
+            run_writer.add_scalar('Model 1 - Loss/val', val_loss, epoch)
 
+        
         #training Model 2
         for epoch in tqdm(range(args.epochs2), desc='Training Model 2',ascii=True):
             model1.load_state_dict(torch.load(path + 'checkpoint-best-loss-model-1.pkl'))
@@ -173,12 +181,16 @@ if __name__ == '__main__':
 
             train_loss = train_M2(model=model2, graphs=graphs, E_meta=E_meta, loss_fn=new_loss, optimizer=optimizer2)
             val_loss, val_acc = infer_M2(model=model2, graphs=graphs, E_meta=E_meta, loss_fn=new_loss, infer_type='val')
-            if (epoch+1)%5 == 0 or epoch == 0:
-                print(f"Epoch {epoch+1}/{args.epochs2} - Train Loss: {train_loss:.4f}, Val Loss: {val_loss:.4f}, Val Accuracy: {val_acc:.4f}")
+            #if (epoch+1)%5 == 0 or epoch == 0:
+                
             if val_loss < best_val_loss_M2 or epoch == 0:
+                print(f"Epoch {epoch+1}/{args.epochs2} - Train Loss: {train_loss:.4f}, Val Loss: {val_loss:.4f}, Val Accuracy: {val_acc:.4f}")
                 best_val_loss_M2 = val_loss
                 torch.save(model2.state_dict(), path + 'checkpoint-best-loss-model-2.pkl')
             val_loss_history_M2.append(val_loss)
+            run_writer.add_scalar('Model 2 - Loss/train', train_loss, epoch)
+            run_writer.add_scalar('Model 2 - Loss/val', val_loss, epoch)
+            run_writer.add_scalar('Model 2 - Accuracy/val', val_acc, epoch)
 
         #testing Model 1 and 2
         print("Testing Model 1 and 2")
@@ -186,6 +198,7 @@ if __name__ == '__main__':
         model2.load_state_dict(torch.load(path + 'checkpoint-best-loss-model-2.pkl'))
         E_meta, test_loss = infer_M1(model=model1, x=coarsen_features, edge_index=coarsen_edge, mask=coarsen_test_mask, y=coarsen_test_labels, loss_fn=F.l1_loss)
         test_loss, test_acc = infer_M2(model=model2, graphs=graphs, E_meta=E_meta, loss_fn=new_loss, infer_type='test')
+        writer.add_scalar('Model 2 - Accuracy/test', test_acc, i)
         all_acc.append(test_acc)
         print(f"Run {i+1}/{args.runs} - Test Loss: {test_loss:.4f}, Test Accuracy: {test_acc:.4f}")
         print("#####################################################################")

utils.py
@@ -20,10 +20,17 @@ def create_distribution_tensor(input_tensor, class_count):
 def one_hot(x, class_count):
     return torch.eye(class_count)[x, :]
 
-def neighbour(G, node):
-    edges_connected_to_k = torch.nonzero(G.edge_index[0] == node, as_tuple=False)
-    neighbors_k = G.edge_index[1][edges_connected_to_k].flatten().tolist()
-    return neighbors_k
+def neighbor(G, nodes):
+    if not hasattr(G, 'edge_index'):
+        raise ValueError("The input graph must have the 'edge_index' attribute.")
+    if not isinstance(nodes, torch.Tensor):
+        nodes = torch.tensor(nodes)
+    nodes = nodes.to(device)
+    edge_index_gpu = G.edge_index.to(nodes.device)
+    mask = (edge_index_gpu[0] == nodes.unsqueeze(-1)).any(dim=0)
+    edges_connected_to_nodes = edge_index_gpu[:, mask]
+    neighbors = torch.unique(edges_connected_to_nodes[1])
+    return neighbors
 
 def extract_components(H):
         if H.A.shape[0] != H.A.shape[1]:
@@ -85,7 +92,7 @@ def metanode_to_node_mapping_new(map_dict, orig_dict):
             temp[metanode].append(orig_dict[node])
     return temp
 
-def coarsening(dataset, coarsening_ratio, coarsening_method):
+def coarsening(args, dataset, coarsening_ratio, coarsening_method):
     if dataset == 'dblp':
         dataset = CitationFull(root='./dataset', name=dataset)
     elif dataset == 'Physics':
@@ -93,13 +100,18 @@ def coarsening(dataset, coarsening_ratio, coarsening_method):
     else:
         dataset = Planetoid(root='./dataset', name=dataset)
     data = dataset[0]
+    num_classes = len(set(np.array(data.y)))
+    if args.normalize_features:
+        data.x = torch.nn.functional.normalize(data.x, p=1)
     G = gsp.graphs.Graph(W=to_dense_adj(data.edge_index)[0])
     components = extract_components(G)
     candidate = sorted(components, key=lambda x: len(x.info['orig_idx']), reverse=True)
+    data.to(device)
     number = 0
     C_list=[]
     Gc_list=[]
-    map_list=[]
+    subgraph_list=[]
+    coarsen_node = 0
     while number < len(candidate):
         H = candidate[number]
         original_map = orig_to_new_map(H.info['orig_idx'])
@@ -107,14 +119,34 @@ def coarsening(dataset, coarsening_ratio, coarsening_method):
             C, Gc, Call, Gall, mapping_dict_list = coarsen(H, r=coarsening_ratio, method=coarsening_method)
             C_list.append(C)
             Gc_list.append(Gc)
-            map_list.append(metanode_to_node_mapping_new(subgraph_mapping(mapping_dict_list), original_map))
+            mapping_dict = metanode_to_node_mapping_new(subgraph_mapping(mapping_dict_list), original_map)
         else:
             C = sp.sparse.eye(H.N, format="csc")
             C_list.append(C)
             Gc_list.append(H)
-            map_list.append(metanode_to_node_mapping_new(subgraph_mapping([{0: 0}]), original_map))
+            mapping_dict = metanode_to_node_mapping_new(subgraph_mapping([{0: 0}]), original_map)
+        for key, value in mapping_dict.items():
+            value = torch.LongTensor(value).to(device)
+            if args.extra_node:
+                ext_nodes = neighbor(data, value)
+                actual_ext = ext_nodes[~torch.isin(ext_nodes, value)].to(device)
+                value = torch.cat((value, ext_nodes), dim=0)
+                value = torch.unique(value)
+            value, _ = torch.sort(value)
+            mappiing = {}
+            for i in range(len(value)):
+                mappiing[value[i].item()] = i
+            M = data.subgraph(value)
+            M.meta_idx = key+coarsen_node
+            M.num_classes = num_classes
+            M.map_dict = mappiing
+            M.ext_node = actual_ext
+            M.to(device)
+            subgraph_list.append(M)
+        coarsen_node += Gc.N
         number += 1
-    return data.x.shape[1], len(set(np.array(data.y))), candidate, C_list, Gc_list, map_list
+    print("Subgraphs created, number of subgraphs: ", len(subgraph_list))
+    return data.x.shape[1], num_classes, candidate, C_list, Gc_list, subgraph_list
 
 def index_to_mask(index, size):
     mask = torch.zeros(size, dtype=torch.bool, device=index.device)
@@ -144,7 +176,7 @@ def splits(data, num_classes, exp):
 
     return data
 
-def load_data(args, dataset, candidate, C_list, Gc_list, exp, map_list):
+def load_data(args, dataset, candidate, C_list, Gc_list, exp, subgraph_list):
     if dataset == 'dblp':
         dataset = CitationFull(root='./dataset', name=dataset)
     elif dataset == 'Physics':
@@ -171,39 +203,37 @@ def load_data(args, dataset, candidate, C_list, Gc_list, exp, map_list):
     coarsen_test_labels = torch.Tensor([])
     coarsen_test_mask = torch.Tensor([]).bool()
 
-    subgraph_list = []
+    new_graphs = []
+
+    for graph in subgraph_list:
+        F = Data(x=graph.x, edge_index=graph.edge_index, y=graph.y, meta_idx=graph.meta_idx, num_classes=graph.num_classes)
+        F.train_mask = torch.zeros(graph.x.shape[0], dtype=torch.bool)
+        F.val_mask = torch.zeros(graph.x.shape[0], dtype=torch.bool)
+        F.test_mask = torch.zeros(graph.x.shape[0], dtype=torch.bool)
+        for node, new_node in graph.map_dict.items():
+            if train_mask[node]:
+                F.train_mask[new_node] = True
+            if val_mask[node]:
+                F.val_mask[new_node] = True
+            if test_mask[node]:
+                F.test_mask[new_node] = True
+            if node in graph.ext_node:
+                F.train_mask[new_node] = False
+                F.val_mask[new_node] = False
+                F.test_mask[new_node] = False
+        
+        new_graphs.append(F)
 
     while number < len(candidate):
         H = candidate[number]
         C = C_list[number]
         Gc = Gc_list[number]
-        mapping_dict = map_list[number]
         keep = H.info['orig_idx']
         H_features = features[keep]
         H_labels = labels[keep]
         H_train_mask = train_mask[keep]
         H_val_mask = val_mask[keep]
         H_test_mask = test_mask[keep]
-
-        for key, value in mapping_dict.items():
-            ext_nodes = []
-            if args.extra_node:
-                for node in value:
-                    ext_nodes.extend(neighbour(data, node))
-                ext_nodes = list(set(ext_nodes))
-                value.extend(ext_nodes)
-                value = list(set(value))
-            value = torch.LongTensor(value)
-            M = data.subgraph(index_to_mask(value, data.num_nodes))
-            M.meta_idx = key+coarsen_node
-            M.num_classes = n_classes
-            subgraph_map_dict = {int(v): i for i, v in enumerate(value)}
-            for node, new_node in subgraph_map_dict.items():
-                if node in ext_nodes:
-                    M.train_mask[new_node] = False
-                    M.val_mask[new_node] = False
-                    M.test_mask[new_node] = False
-            subgraph_list.append(M)
 
         #if len(H.info['orig_idx']) > 10 and torch.sum(H_train_mask)+torch.sum(H_val_mask) > 0:
         if torch.sum(H_train_mask)+torch.sum(H_val_mask) > 0:
@@ -283,4 +313,4 @@ def load_data(args, dataset, candidate, C_list, Gc_list, exp, map_list):
     coarsen_val_labels = coarsen_val_labels.long()
     coarsen_test_labels = coarsen_test_labels.long()
 
-    return data, coarsen_features, coarsen_train_labels, coarsen_train_mask, coarsen_val_labels, coarsen_val_mask, coarsen_test_labels, coarsen_test_mask, coarsen_edge, subgraph_list
+    return coarsen_features, coarsen_train_labels, coarsen_train_mask, coarsen_val_labels, coarsen_val_mask, coarsen_test_labels, coarsen_test_mask, coarsen_edge, new_graphs