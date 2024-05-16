0084df56b665f512ab4160733ecc20f60c3928fa
Robohrriday <robohrriday.8@gmail.com>
2024-05-16 15:27:51 +0530

    added results.csv

--- get_graph_data_analysis.sh
+++ get_graph_data_analysis.sh
@@ -1,5 +1,5 @@
 #run graph_data_analysis.py for all datasets and coarsening ratios
-for dataset in citeseer pubmed dblp Physics
+for dataset in pubmed dblp Physics
 do
     for coarsening_method in variation_neighborhoods
     do
